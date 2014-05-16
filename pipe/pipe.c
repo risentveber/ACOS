863ae62b6f3b498b5173f155f92ee40654fdfa5c
Risent Veber <bsstr@yandex.ru>
2014-05-16 20:58:58 +0400

    some thing that doesn't matter

--- pipe/pipe.c
+++ pipe/pipe.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 
 const int BUFF_SIZE = 1000000;
+const int max_size = 1000000000;
 const int ex_count = 100;
 char * buff;
 
@@ -78,14 +79,14 @@ int main(int argc, char const *argv[])
 		long long int diff;
 		int num = 0;
 		printf("%5s%10s%15s%10s\n", "num", "size","average_time", "speed");
-		for (i = 1; i <= BUFF_SIZE; i = i*2){
+		for (i = 1; i <= max_size; i = i*2){
 			num++;
 			gettimeofday(&t1, NULL);
 			for(c = 0; c < ex_count; c++){
 				read_from_pipe(fd_read, i);
 			}
 			gettimeofday(&t2, NULL);
-			diff = t2.tv_usec - t1.tv_usec;
+			diff = (t2.tv_sec - t1.tv_sec)*1000000 + t2.tv_usec - t1.tv_usec;
 			printf("%5d%10lld%15lf%10lf \n", num, i, (double)(diff)/ex_count, ((double)(diff))/ex_count/i);
 		}
 		close(fd_read);
@@ -93,7 +94,7 @@ int main(int argc, char const *argv[])
 		close(p[0]);//close end to read 
 		int fd_write = p[1];
 
-		for (i = 1; i <= BUFF_SIZE; i = i*2){
+		for (i = 1; i <= max_size; i = i*2){
 			for(c = 0; c < ex_count; c++){
 				write_to_pipe(fd_write, i);
 			}
